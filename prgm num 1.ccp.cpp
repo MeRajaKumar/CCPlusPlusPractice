c6810b2e80cd25df385ee9b2877953834b54adef
MeRajaKumar <teamaayushgupta@gmail.com>
2023-10-23 17:49:05 +0530

    updated repo

---
+++ prgm num 1.ccp.cpp
@@ -0,0 +1,25 @@
+// Qus - Star pettern
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n,s,i,j;
+    cout<<"enter number for rows:";
+    cin>>n;
+    for(i=0;i<=n;i++){
+        for(s=n;s>i;s--)
+        cout<<" ";
+        for(j=0;j<i;j++)
+        cout<<"* ";
+        cout<<"\n ";
+    }
+    for(i=1;i<n;i++){
+        for(s=0;s<i;s++)
+        cout<<" ";
+        for(j=n;j>i;j--)
+        cout<<"* ";
+        //ending line after each row
+        cout<<"\n ";
+    }
+    return 0;
+}
